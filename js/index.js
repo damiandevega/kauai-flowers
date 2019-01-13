b13749f91c19c0f5e641729363a347c6b2ff568d
Damian de Vega <damiandevega@gmail.com>
2019-01-13 01:30:23 -0500

    fixing a js error with the slick call and modified some html for better performance

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
-// Lightbox Init
+// Lightbox init
 $(document).on('click', '[data-toggle="lightbox"]', function (event) {
     event.preventDefault();
   $(this).ekkoLightbox();
 });
 
-$('.slider').slick({
-    infinite: true,
-    slideToShow: 1,
-    slideToScroll: 1
-});
+// Testimonial slider
+if ($('.slider').length === 1) {
+    $('.slider').slick({
+        infinite: true,
+        slideToShow: 1,
+        slideToScroll: 1
+    });
+}
